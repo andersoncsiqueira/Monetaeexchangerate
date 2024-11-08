f395fb6fcd6c9ffc37ee3568e47a9b331a72e148
Anderson Siqueira <92267959+andersoncsiqueira@users.noreply.github.com>
2024-11-08 10:52:48 -0300

    mudule

--- js/api.js
+++ js/api.js
@@ -6,8 +6,9 @@
  const getPrice = async (urlLastPrice) => {
     const response = await fetch(urlLastPrice)
     const datas = await response.json() 
+    let bid = datas.USDBRL.bid
 
-return datas
+return bid
 
 }
 
