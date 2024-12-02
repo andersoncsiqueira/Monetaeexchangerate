9f4173cd47d5ca3bcf7db33c21245bc0a6cad043
Anderson Siqueira <92267959+andersoncsiqueira@users.noreply.github.com>
2024-12-02 09:27:29 -0300

    Update util.js

--- js/util.js
+++ js/util.js
@@ -1,8 +1,8 @@
 import { urlLastUp } from "./api.js";
 
-const coins = [,'USD','EUR','CAD','GBP','AUD','ARS','CLP','CHF']
-const spreadsSell = {'USD':1.06, 'EUR': 1.06, 'CAD':1.09,'GBP': 1.077,'AUD':1.09,'ARS': 1.75,'CLP': 1.35,'CHF':1.1}
-const spreadCall =  {'USD':1.25, 'EUR': 1.25, 'CAD':1.25,'GBP': 1.2,'AUD':1.25,'ARS': 1.75,'CLP': 1.25,'CHF':1.25}
+const coins = [,'USD','EUR','CAD','GBP','AUD','ARS','CLP','CHF','NZD']
+const spreadsSell = {'USD':1.06, 'EUR': 1.06, 'CAD':1.09,'GBP': 1.077,'AUD':1.09,'ARS': 1.75,'CLP': 1.35,'CHF':1.1,'NZD':1.077}
+const spreadCall =  {'USD':1.25, 'EUR': 1.25, 'CAD':1.25,'GBP': 1.2,'AUD':1.25,'ARS': 1.75,'CLP': 1.25,'CHF':1.25,'NZD':1.25}
 const iof = 1.011
   const mathOfSell =  async (getPrice,counterCoin,spread) => {
     let exchangeRate = null
