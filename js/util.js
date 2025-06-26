cb5293018fdbb4c18049049ee0e4b1f0db7c6da1
Anderson Siqueira <92267959+andersoncsiqueira@users.noreply.github.com>
2025-06-26 09:03:19 -0300

    iof 1.1

--- js/util.js
+++ js/util.js
@@ -3,7 +3,7 @@ import { getPrice, newApiBaseUrl } from "./api.js";
 const coins = ['USD','EUR','CAD','GBP','AUD','ARS','CLP','CHF','NZD']
 const spreadsSell = {'USD':1.06, 'EUR': 1.06, 'CAD':1.09,'GBP': 1.077,'AUD':1.09,'ARS': 1.75,'CLP': 1.35,'CHF':1.1,'NZD':1.077}
 const spreadCall =  {'USD':1.25, 'EUR': 1.25, 'CAD':1.25,'GBP': 1.2,'AUD':1.25,'ARS': 1.75,'CLP': 1.75,'CHF':1.25,'NZD':1.25}
-const iof = 1.035
+const iof = 1.011
 
 
 
